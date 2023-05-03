e2974e202b505f25408fe3a3598a90b36bb39d28
MischaelP <phemius.p@gmail.com>
2023-05-03 16:04:24 +0200

    Fusion des elements

---
+++ countdown/countdown.js
@@ -0,0 +1,31 @@
+
+ function startCountdown(durationInSeconds) {
+    let timer = durationInSeconds;
+    const countdownInterval = setInterval(function () {
+      const minutes = Math.floor(timer / 60);
+      const seconds = Math.floor(timer % 60);
+  
+      console.log(`${minutes}:${seconds}`);
+  
+      timer--;
+  
+      if (timer < 0) {
+        clearInterval(countdownInterval);
+        console.log('Time is up!');
+      }
+    }, 1000);
+  } 
+  // USER CHOISIT DANS MENU 30MIN,60MIN,1H ET LE JS VA CONVERTIR EN SECONDE ET RENTRER LA DEMANDE EN PARAMÈTRE DE LA FONCTION
+  // HTML = 30MN, 60MN, 120MN, ETC
+  // => JS = 1800S, 3600S, 7200S
+  
+ // La fonction startCountdown prend en paramètre la durée totale du timer en secondes. 
+ //Elle initialise une variable timer à cette valeur et crée un intervalle de temps (setInterval) 
+ //qui se répète toutes les secondes (grâce au délai de 1000ms passé en dernier paramètre).
+
+ //À chaque itération, la fonction calcule le nombre de minutes et de secondes restantes en divisant le temps restant par 60.
+ // Elle affiche ensuite ces informations dans la console avec console.log().
+
+ //La variable timer est décrémentée de 1 à chaque itération jusqu'à atteindre 0.
+ // Lorsque timer atteint 0, l'intervalle est nettoyé avec clearInterval() et un message "Time is up!" est affiché dans la console.
+//if (document.getElementById('30') 
